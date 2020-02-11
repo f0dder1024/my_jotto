75bd642f7a111c299f31a813c2c9978bb9bc1578
f0dder1024 <mlee1024@gmail.com>
2020-02-11 01:43:31 -0800

    Add CORRECT_GUESS action creator and tests

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+export const actionTypes = {
+  CORRECT_GUESS: "CORRECT_GUESS"
+};
+
+/**
+ * @function correctGuess
+ * @returns {object} - Action object with type 'CORRECT_GUESS'
+ */
+const correctGuess = () => {
+  return {
+    type: actionTypes.CORRECT_GUESS
+  };
+};
+
+export { correctGuess };
